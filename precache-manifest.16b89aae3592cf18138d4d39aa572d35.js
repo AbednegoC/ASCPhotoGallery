0748162f338137e36772e6e86972b46934bbf73b
Abednego Chase <achase@Abednegos-MacBook-Pro.local>
2020-08-31 16:05:28 -0400

    Updates

---
+++ precache-manifest.16b89aae3592cf18138d4d39aa572d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860860e7622cd08ed521f2356f34dfe1",
+    "url": "/ASCPhotoGallery/index.html"
+  },
+  {
+    "revision": "ce057687251edac4af12",
+    "url": "/ASCPhotoGallery/static/css/main.9cb5a782.chunk.css"
+  },
+  {
+    "revision": "5a3bd44839ce7cc7c684",
+    "url": "/ASCPhotoGallery/static/js/2.4d6ab519.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/ASCPhotoGallery/static/js/2.4d6ab519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce057687251edac4af12",
+    "url": "/ASCPhotoGallery/static/js/main.d5430ae5.chunk.js"
+  },
+  {
+    "revision": "8513be07232e4957ef0e",
+    "url": "/ASCPhotoGallery/static/js/runtime-main.b6eb94b8.js"
+  }
+]);
